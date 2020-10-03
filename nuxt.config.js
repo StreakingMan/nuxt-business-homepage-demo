22f5d7021cdb4239e45ff77363076921aa07ada6
streakingMan <z_max_y@163.com>
2020-10-03 21:11:40 +0800

    chore: 端口更改

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,7 +74,7 @@ export default {
 
     // Server
     server: {
-        port: 8000, // default: 3000
+        port: 80, // default: 3000
         host: '0.0.0.0', // default: localhost,
     },
 }
