f284abdf7818ae0bc3734652f65d5575341e8911
streakingMan <z_max_y@163.com>
2020-10-15 16:42:56 +0800

    chore: 更改分线卡片标题描述

--- layouts/default.vue
+++ layouts/default.vue
@@ -212,8 +212,8 @@ export default {
                 // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
                 // eslint-disable-next-line no-undef
                 wx.updateAppMessageShareData({
-                    title: '星瀚投资微官网', // 分享标题
-                    desc: '杭州星瀚投资管理有限公司', // 分享描述
+                    title: '星瀚微官网', // 分享标题
+                    desc: '杭州星瀚微官网', // 分享描述
                     link:
                         process.env.NODE_ENV === 'development'
                             ? 'http://192.168.0.104:8000/'
@@ -225,7 +225,7 @@ export default {
                 })
                 // eslint-disable-next-line no-undef
                 wx.updateTimelineShareData({
-                    title: '杭州星瀚投资管理有限公司', // 分享标题
+                    title: '星瀚微官网', // 分享标题
                     link:
                         process.env.NODE_ENV === 'development'
                             ? 'http://192.168.0.104:8000/'
